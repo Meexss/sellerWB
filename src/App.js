0a83fef04b079e197a19fa30613b3eab170648e2
Maksim Pavlov <maksimpavlov9876@gmail.com>
2023-03-21 17:52:24 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import Label from "./component/Label";
 
 function App() {
+
+  const [order, setOrder] = useState([]); //данные по поставке
+  const [sortData, setSortData] = useState([]); //сортированные данные поставки
+  const [seconds, setSeconds] = useState(0); // счетчик
+
+  const initOrder = async () => {
+    const response = await fetch(
+        'https://suppliers-api.wildberries.ru/api/v3/supplies/WB-GI-42354428/orders',
+        {
+          headers: {
+            Accept: 'application/json',
+            Authorization:
+                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjY0MDU3NzQwLThkMzYtNGI0YS1iZjZhLTlkMmEzMDljM2Q4NyJ9.OMO-inzyzCUil3VcsP2SjrsiiaQSOjCR1ezgPgsB8vw',
+          },
+        }
+    )
+        .then((response) => response.json())
+        .then((orders) => setOrder(orders.orders));
+  };
+
+  useEffect(() => {
+    initOrder();
+  }, []);
+
+  useEffect(() => {
+    setSortData(order.sort(byField('article')))
+  }, [order.length >= 1]);
+
+  function byField(field) {
+    return (a, b) => a[field] > b[field] ? 1 : -1;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {sortData
+                ?   sortData.map((item, index) => <Label key={item.id} data={item} ind={index}/>
+          )
+
+                : <span>Загружаем данные поставки</span>
+      }
+
     </div>
   );
 }
