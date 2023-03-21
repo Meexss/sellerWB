3afcc97c57840566eee4881c48cc698934fbe65a
Maksim Pavlov <maksimpavlov9876@gmail.com>
2023-03-21 21:43:17 +0300

    done print

--- src/pages/LayOut.jsx
+++ src/pages/LayOut.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {Outlet} from "react-router-dom";
 import Header from "../component/Header/Header";
 import classes from "./LayOut.module.css";
 import Footer from "../component/Footer/Footer";
+import ReactToPrint from "react-to-print";
 
 const LayOut = () => {
+    const ref = useRef()
     return (
         <div>
             <Header/>
+            <ReactToPrint content={() => ref.current} trigger={() => <button className={classes.print}>Print</button>}/>
             <div className={classes.wrapper}></div>
-            <div className={classes.wrapperOutlet}>
-                <Outlet/>
+            <div ref={ref} className={classes.wrapperOutlet}>
+                <Outlet />
             </div>
             <Footer/>
         </div>
